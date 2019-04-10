b93de1d9f9dd415943e90e7c3776ed2fd4e17b6d
Max Stoiber <mxstbr@github.com>
2019-04-10 10:29:16 +0200

    Add repo limiter select

--- github-repository-fetcher/src/views/RepositoryList/index.js
+++ github-repository-fetcher/src/views/RepositoryList/index.js
@@ -10,23 +10,50 @@ const Repository = ({ repository }) => {
   );
 };
 
-export default ({ match }) => (
-  <FetchRepositories username={match.params.username}>
-    {({ error, loading, data }) => {
-      if (data) {
-        return (
-          <div>
-            <h2>@{match.params.username}'s repositories</h2>
-            {data.map(repository => (
-              <Repository key={repository.id} repository={repository} />
-            ))}
-          </div>
-        );
-      }
-      if (loading) return <Spinner />;
-      if (error) return <div>{error}</div>;
+export default class RepositoryList extends React.Component {
+  state = {
+    amount: "5"
+  };
 
-      return null;
-    }}
-  </FetchRepositories>
-);
+  render() {
+    const { match } = this.props;
+    return (
+      <div>
+        <div>
+          Show
+          <select
+            value={this.state.amount}
+            onChange={evt =>
+              this.setState({
+                amount: evt.target.value
+              })
+            }
+          >
+            <option value="5">5 repos</option>
+            <option value="10">10 repos</option>
+          </select>
+        </div>
+        <FetchRepositories username={match.params.username}>
+          {({ error, loading, data }) => {
+            if (data) {
+              return (
+                <div>
+                  <h2>@{match.params.username}'s repositories</h2>
+                  {data
+                    .slice(0, Number(this.state.amount))
+                    .map((repository, index) => (
+                      <Repository key={repository.id} repository={repository} />
+                    ))}
+                </div>
+              );
+            }
+            if (loading) return <Spinner />;
+            if (error) return <div>{error}</div>;
+
+            return null;
+          }}
+        </FetchRepositories>
+      </div>
+    );
+  }
+}
