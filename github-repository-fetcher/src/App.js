7ba2f979e5273177949f4115a87ce8f60dd6b48c
Max Stoiber <mxstbr@github.com>
2019-04-11 16:45:43 +0200

    Code split with suspense

--- github-repository-fetcher/src/App.js
+++ github-repository-fetcher/src/App.js
@@ -1,17 +1,25 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Spinner from "./components/Spinner";
 import SearchHistoryProvider from "./SearchHistory";
-import Home from "./views/Home";
-import RepositoryList from "./views/RepositoryList";
+
+const Home = React.lazy(() =>
+  import("./views/Home" /* webpackChunkName: "Home" */)
+);
+const RepositoryList = React.lazy(() =>
+  import("./views/RepositoryList" /* webpackChunkName: "RepositoryList" */)
+);
 
 export default () => (
   <div>
     <SearchHistoryProvider>
       <BrowserRouter>
-        <Switch>
-          <Route path="/:username" component={RepositoryList} />
-          <Route path="/" component={Home} />
-        </Switch>
+        <React.Suspense fallback={<Spinner />}>
+          <Switch>
+            <Route path="/:username" component={RepositoryList} />
+            <Route path="/" component={Home} />
+          </Switch>
+        </React.Suspense>
       </BrowserRouter>
     </SearchHistoryProvider>
   </div>
